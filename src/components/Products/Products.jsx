905226dd756ea3e099f0bf908d2eed8942d2ac48
nayebislam <nayebislamnafi475@gmail.com>
2025-06-20 19:51:00 +0600

    Products top part complete

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import Containar from "../../layout/Containar";
+import MainHeading from "../../layout/MainHeading";
+import SubHeading from "../../layout/SubHeading";
 
 const Products = () => {
   return (
-    <div>Products</div>
-  )
-}
+    <section>
+      <Containar>
+        <div className="flex items-end justify-between">
+          <div>
+            <MainHeading>This Month</MainHeading>
+            <SubHeading>Best Selling Products</SubHeading>
+          </div>
+          <div>
+            <button className="py-4 px-12 font-primary font-medium leading-6 bg-primary text-white rounded">
+              View All
+            </button>
+          </div>
+        </div>
+      </Containar>
+    </section>
+  );
+};
 
-export default Products
+export default Products;
