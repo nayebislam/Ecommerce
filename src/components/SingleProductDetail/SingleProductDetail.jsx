f4331cfcaa7cbedaa2926e8f6e306a27560852e9
nayebislam <nayebislamnafi475@gmail.com>
2025-07-02 14:32:44 +0600

    Product Details Page Update

--- src/components/SingleProductDetail/SingleProductDetail.jsx
+++ src/components/SingleProductDetail/SingleProductDetail.jsx
@@ -1,44 +1,63 @@
-import React from "react";
-import main from "../../assets/main.png";
-import p1 from "../../assets/p1.png";
-import p2 from "../../assets/p2.png";
-import p3 from "../../assets/p3.png";
-import p4 from "../../assets/p4.png";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import SingleProductRating from "./SingleProductRating";
 
 const SingleProductDetail = () => {
+  const {id} = useParams()
+  const [productData, setProductData] = useState([]);
+  const singleProduct = productData.find((product) => product.id == id)
+  
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((data) => setProductData(data.products));
+  }, []);
+
   return (
     <div className="pb-[140px]">
       <div className="py-[80px]">
         <p className="font-primary text-[14px] leading-[21px] text-[#808080]">
-          Account / Gaming /
-          <span className="font-primary text-[14px] leading-[21px] text-black">
-            Havic HV G-92 Gamepad
+          Account / Gaming / 
+          <span className="font-primary text-[14px] leading-[21px] text-black ml-1.5">
+            {singleProduct?.title}
           </span>
         </p>
       </div>
-      <div className="grid grid-cols-12 space-x-5">
+      <div className="grid grid-cols-12">
         <div className="col-span-2">
-          <div className="flex flex-col gap-y-3">
-            <div className="py-3 px-5 bg-[#F5F5F5] rounded flex justify-center items-center">
-              <img src={p1} alt="" />
+          <div className="flex flex-col gap-y-3 mr-[30px]">
+            <div className="py-1.5 px-5 bg-[#F5F5F5] rounded flex justify-center items-center">
+              <img src={singleProduct?.images[0]} alt="" />
             </div>
-            <div className="py-3 px-5 bg-[#F5F5F5] rounded flex justify-center items-center">
-              <img src={p2} alt="" />
+            <div className="py-1.5 px-5 bg-[#F5F5F5] rounded flex justify-center items-center">
+              <img src={singleProduct?.images[0]} alt="" />
             </div>
-            <div className="py-3 px-5 bg-[#F5F5F5] rounded flex justify-center items-center">
-              <img src={p3} alt="" />
+            <div className="py-1.5 px-5 bg-[#F5F5F5] rounded flex justify-center items-center">
+              <img src={singleProduct?.images[0]} alt="" />
             </div>
-            <div className="py-3 px-5 bg-[#F5F5F5] rounded flex justify-center items-center">
-              <img src={p4} alt="" />
+            <div className="py-1.5 px-5 bg-[#F5F5F5] rounded flex justify-center items-center">
+              <img src={singleProduct?.images[0]} alt="" />
             </div>
           </div>
         </div>
-        <div className="col-span-6">
-          <div className="pt-[133px] pb-[104px] px-[27px] bg-[#F5F5F5] rounded">
-            <img src={main} alt="" />
+        <div className="col-span-6 mr-[70px]">
+          <div className="pt-[133px] pb-[152px] px-[27px] bg-[#F5F5F5] rounded flex items-center justify-center">
+            <img src={singleProduct?.thumbnail} alt="" />
           </div>
         </div>
-        <div className="col-span-4">sDFoug</div>
+        <div className="col-span-4">
+          <h3 className="font-secondary font-semibold text-2xl leading-6 text-black">{singleProduct?.title}</h3>
+          <div className="flex items-center my-4">
+            <SingleProductRating rating={singleProduct?.rating} />
+            <p className="ml-2 font-primary text-[14px] leading-[21px] text-[#808080]">{`(${singleProduct?.reviews.length}Reviews)`}</p>
+            <p className="px-4 text-[#808080]">|</p>
+            <p className="font-primary text-[14px] leading-[21px] text-[#00FF66]">In Stock</p>
+          </div>
+          <p className="font-secondary text-2xl leading-6 tracking-[3%]">${singleProduct?.price}</p>
+          <p className="font-primary text-[14px] leading-[21px] text-black max-w-[373px] mt-6">{singleProduct?.description}</p>
+          <div className="border border-[#808080] my-6"></div>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/SingleProductDetail/SingleProductRating.jsx
@@ -0,0 +1,23 @@
+import { TiStarFullOutline } from "react-icons/ti";
+import { FaRegStarHalfStroke } from "react-icons/fa6";
+import { FaRegStar } from "react-icons/fa";
+
+const SingleProductRating = ({ rating }) => {
+  const FullRating = Math.floor(rating);
+  const HalfRating = rating % 1 >= 0.5;
+  const EmptyRating = 5 - FullRating - (HalfRating ? 1 : 0);
+
+  return (
+    <div className="flex items-center">
+      {[...Array(FullRating)].map(() => (
+        <TiStarFullOutline size={20} color="#FFAD33" />
+      ))}
+      {HalfRating && <FaRegStarHalfStroke size={20} color="#FFAD33" />}
+      {[...Array(EmptyRating)].map(() => (
+        <FaRegStar size={20} color="#EFEFEF" />
+      ))}
+    </div>
+  );
+};
+
+export default SingleProductRating;
