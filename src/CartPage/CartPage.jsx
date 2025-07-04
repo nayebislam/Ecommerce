a2bf2fdff4736bb8a4ccc86e933a66bfeb9540a7
nayebislam <nayebislamnafi475@gmail.com>
2025-07-04 16:30:17 +0600

    cart part update

--- src/CartPage/CartPage.jsx
+++ src/CartPage/CartPage.jsx
@@ -1,9 +1,100 @@
-import React from 'react'
+import React from "react";
+import p4 from "../assets/p4.png";
+import { IoChevronUpSharp } from "react-icons/io5";
+import { IoChevronDownSharp } from "react-icons/io5";
 
 const CartPage = () => {
   return (
-    <div>CartPage</div>
-  )
-}
+    <div>
+      <div className="flex items-center py-[80px]">
+        <p className="font-primary text-[14px] leading-[21px] text-[#808080]">
+          Home
+        </p>
+        <span className="px-3">/</span>
+        <p className="font-primary text-[14px] leading-[21px] text-black">
+          Cart
+        </p>
+      </div>
+      <div className=" flex flex-col gap-y-[40px] mb-6">
+        <div className="flex items-center justify-between py-6 px-[40px] rounded shadow-[0_1px_13px_rgba(0,0,0,0.25)]">
+          <div className="w-[25%] text-start font-primary text-[16px] leading-[24px] text-black">
+            Product
+          </div>
+          <div className="w-[25%] text-center font-primary text-[16px] leading-[24px] text-black">
+            Price
+          </div>
+          <div className="w-[25%] text-center font-primary text-[16px] leading-[24px] text-black">
+            Quantity
+          </div>
+          <div className="w-[25%] text-end font-primary text-[16px] leading-[24px] text-black">
+            Subtotal
+          </div>
+        </div>
+        <div className="flex items-center justify-between py-6 px-[40px] rounded shadow-[0_1px_13px_rgba(0,0,0,0.25)]">
+          <div className="w-[25%] text-center flex gap-x-5 items-center">
+            <img className="w-[50px]" src={p4} alt="" />
+            <p className="font-primary text-[16px] leading-[24px] text-black">
+              H1 Gamepad
+            </p>
+          </div>
+          <div className="w-[25%] text-center font-primary text-[16px] leading-[24px] text-black">
+            $650
+          </div>
+          <div className="w-[25%] text-center">
+            <div className="py-2.5 px-3 border border-[#999999] rounded inline-block">
+              <div className="flex items-center gap-x-4">
+                <p className="font-primary text-[16px] leading-[24px] text-black">
+                  01
+                </p>
+                <div>
+                  <IoChevronUpSharp size={16} />
+                  <IoChevronDownSharp size={16} />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-[25%] text-end font-primary text-[16px] leading-[24px] text-black">
+            $650
+          </div>
+        </div>
+        <div className="flex items-center justify-between py-6 px-[40px] rounded shadow-[0_1px_13px_rgba(0,0,0,0.25)]">
+          <div className="w-[25%] text-center flex gap-x-5 items-center">
+            <img className="w-[50px]" src={p4} alt="" />
+            <p className="font-primary text-[16px] leading-[24px] text-black">
+              H1 Gamepad
+            </p>
+          </div>
+          <div className="w-[25%] text-center font-primary text-[16px] leading-[24px] text-black">
+            $650
+          </div>
+          <div className="w-[25%] text-center">
+            <div className="py-2.5 px-3 border border-[#999999] rounded inline-block">
+              <div className="flex items-center gap-x-4">
+                <p className="font-primary text-[16px] leading-[24px] text-black">
+                  01
+                </p>
+                <div>
+                  <IoChevronUpSharp size={16} />
+                  <IoChevronDownSharp size={16} />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-[25%] text-end font-primary text-[16px] leading-[24px] text-black">
+            $650
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center justify-between mb-[80px]">
+        <button className="py-4 px-12 rounded border border-[#808080] font-primary font-medium text-[16px] leading-[24px] text-black">
+          Return To Shop
+        </button>
+        <button className="py-4 px-12 rounded border border-[#808080] font-primary font-medium text-[16px] leading-[24px] text-black">
+          Update Cart
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default CartPage
+export default CartPage;
