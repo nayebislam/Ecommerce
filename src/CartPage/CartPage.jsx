59aa3e19713a4c793e523e78d40956c950b529ec
nayebislam <nayebislamnafi475@gmail.com>
2025-07-11 23:35:29 +0600

    update

--- src/CartPage/CartPage.jsx
+++ src/CartPage/CartPage.jsx
@@ -111,6 +111,9 @@ const CartPage = () => {
           <button className="py-4 px-8 bg-primary text-white rounded">saehdfi</button>
           <button className="py-4 px-8 bg-primary text-white rounded">saehdfi</button>
           <button className="py-4 px-8 bg-primary text-white rounded">saehdfi</button>
+          <button className="py-4 px-8 bg-primary text-white rounded">saehdfi</button>
+          <button className="py-4 px-8 bg-primary text-white rounded">saehdfi</button>
+          <button className="py-4 px-8 bg-primary text-white rounded">saehdfi</button>
         </div>
       </div>
     </div>
